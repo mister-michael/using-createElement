0d29a3c8fc9718c0fd51aa9ebbad39e4fe7c893d
Michael Clark <michaelclarknashville@protonmail.com>
2020-01-20 14:59:43 -0600

    using createElement I added a section with a class and text content to the html

---
+++ main.js
@@ -0,0 +1,25 @@
+const targetContainer = document.querySelector("#messages");
+
+const section = document.createElement('section');
+section.className = "message"
+section.textContent = "Hi"
+
+targetContainer.appendChild(section);
+
+
+// console.log(createContainer);
+
+// // Create an unordered list element
+// const list = document.createElement('ul')
+
+// // Create a list item for our list
+// const listItem = document.createElement('li')
+// listItem.className = "president"
+// listItem.textContent = "George Bush"
+
+// // Put the list item on the unordered list
+// list.appendChild(listItem)
+
+// console.log(list)
+
+
