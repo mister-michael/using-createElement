586cc0bc68c175e8d1b81b6d89dc8e76d6c39252
Michael Clark <michaelclarknashville@protonmail.com>
2020-01-20 15:32:14 -0600

    appendedchild and added sections to the html

--- main.js
+++ main.js
@@ -1,11 +1,30 @@
 const targetContainer = document.querySelector("#messages");
 
-const section = document.createElement('section');
-section.className = "message"
-section.textContent = "Hi"
+const section1 = document.createElement('section');
+section1.className = "message"
+section1.textContent = "Hi"
 
-targetContainer.appendChild(section);
+const section2 = document.createElement('section');
+section2.className = "message"
+section2.textContent = "Sup"
 
+const section3 = document.createElement('section');
+section3.className = "message"
+section3.textContent = "Nothin"
+
+const section4 = document.createElement('section');
+section4.className = "message"
+section4.textContent = "Huh?"
+
+const section5 = document.createElement('section');
+section5.className = "message"
+section5.textContent = "Nothin's up"
+
+targetContainer.appendChild(section1);
+targetContainer.appendChild(section2);
+targetContainer.appendChild(section3);
+targetContainer.appendChild(section4);
+targetContainer.appendChild(section5);
 
 // console.log(createContainer);
 
